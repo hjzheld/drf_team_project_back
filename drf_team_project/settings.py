0af18d8932becf75f15911eb30d38b1421637a47
hjzheld <dusen7774@naver.com>
2023-10-05 20:22:27 +0900

    📝 user 앱 추가

--- drf_team_project/settings.py
+++ drf_team_project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'user',
 ]
 
 REST_FRAMEWORK = {
@@ -128,3 +129,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+AUTH_USER_MODEL = 'user.User'
+
+STATIC_ROOT = BASE_DIR/"static"
+STATIC_URL = "/static/"
+
+MEDIA_ROOT = BASE_DIR/"media"
+MEDIA_URL = "/media/"
