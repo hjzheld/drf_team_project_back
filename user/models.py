c24fd25b64bef556bf82079464ab1b2adee22ddd
hjzheld <dusen7774@naver.com>
2023-10-05 20:21:37 +0900

    ✨ 회원가입 기능 추가

--- user/models.py
+++ user/models.py
@@ -1,3 +1,69 @@
 from django.db import models
+from django.contrib.auth.models import (
+    BaseUserManager, AbstractBaseUser
+)
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, nickname, mbti, blog, password=None):
+        if not email:
+            raise ValueError('Users must have an email address')
+        if not nickname:
+            raise ValueError('Users must have an email nickname')
+
+        user = self.model(
+            email=self.normalize_email(email),
+            nickname = nickname,
+            mbti = mbti,
+            blog = blog
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, nickname, mbti, blog, password=None):
+        user = self.create_user(
+            email,
+            nickname,
+            mbti,
+            blog,
+            password = password,
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser):
+    email = models.EmailField(
+        verbose_name='email address',
+        max_length=255,
+        unique=True,
+    )
+    nickname = models.CharField(max_length=100, unique=True)
+    is_admin = models.BooleanField(default=False)
+    mbti = models.CharField(max_length=100, null=True, blank=True)
+    blog = models.CharField(max_length=100, null=True, blank=True)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['nickname', 'mbti', 'blog']
+
+
+    def __str__(self):
+        return self.nickname
+
+    def has_perm(self, perm, obj=None):
+        "Does the user have a specific permission?"
+        return True
+
+    def has_module_perms(self, app_label):
+        "Does the user have permissions to view the app `app_label`?"
+        return True
+
+    @property
+    def is_staff(self):
+        "Is the user a member of staff?"
+        return self.is_admin
