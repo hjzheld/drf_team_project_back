aee7107f43e4933c53135db0866ff1c0d26e8ca9
hjzheld <dusen7774@naver.com>
2023-10-11 11:34:30 +0900

    ✨ 좋아요 기능 추가

--- articles/views.py
+++ articles/views.py
@@ -53,4 +53,13 @@ class ArticleDetailView(APIView):
             return Response({'message':'권한이 없습니다.'}, status=status.HTTP_401_UNAUTHORIZED)
 
 
-        
+class LikeView(APIView):
+    def post(self, request, article_id):
+        article = get_object_or_404(Article, id=article_id)
+        if request.user in article.likes.all():
+            article.likes.remove(request.user)
+            return Response('좋아요 취소', status=status.HTTP_200_OK)
+        else:
+            article.likes.add(request.user)
+            return Response('좋아요 완료', status=status.HTTP_200_OK)
+       
